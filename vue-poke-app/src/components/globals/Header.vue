6b6ab178ea61ebc046cb223b55935d298f6aa799
souemon <kobekobekitakita@gmail.com>
2024-08-30 14:54:49 +0900

    update: タイトルから一覧画面に遷移可能にする

--- vue-poke-app/src/components/globals/Header.vue
+++ vue-poke-app/src/components/globals/Header.vue
@@ -5,11 +5,20 @@
     <v-app-bar-title
       class="d-flex justify-start align-center ml-8 font-weight-bold"
     >
-      ポケモン図鑑
+      <router-link :to="{ name: 'list' }">ポケモン図鑑</router-link>
     </v-app-bar-title>
     <v-btn icon="mdi-magnify" size="x-large"></v-btn>
   </v-app-bar>
   <div class="header_margin"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-toolbar-title__placeholder > a {
+  color: black;
+  text-decoration: none;
+}
+
+.v-app-bar-title {
+  height: 80px;
+}
+</style>
