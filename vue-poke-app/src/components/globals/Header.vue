f0a65431e485fa79b22c6c0150feb8d49f6ce62c
souemon <kobekobekitakita@gmail.com>
2024-08-30 13:17:40 +0900

    add: ヘッダーの追加

--- vue-poke-app/src/components/globals/Header.vue
+++ vue-poke-app/src/components/globals/Header.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <v-app-bar color="white" height="80">
+    <v-app-bar-title
+      class="d-flex justify-start align-center ml-8 font-weight-bold"
+    >
+      ポケモン図鑑
+    </v-app-bar-title>
+    <v-btn icon="mdi-magnify" size="x-large"></v-btn>
+  </v-app-bar>
+  <div class="header_margin"></div>
+</template>
 
 <style scoped></style>
