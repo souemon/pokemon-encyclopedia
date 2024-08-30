e978f99e85b7bf63ce1b29da7879402430ae42b5
souemon <kobekobekitakita@gmail.com>
2024-08-30 15:58:36 +0900

    add: 詳細画面の表示機能

--- vue-poke-app/src/router/index.ts
+++ vue-poke-app/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import PokemonListPage from "../components/pages/PokemonListPage.vue";
+import PokemonDetailPage from "../components/pages/PokemonDetailPage.vue";
 
 const routes = [
   { path: "/", name: "list", component: PokemonListPage },
+  { path: "/detail/:id", name: "detail", component: PokemonDetailPage },
   { path: "/:pathMatch(.*)*", redirect: "/" }, // routesに定義されていないパスの場合は一覧画面にリダイレクト
 ];
 
