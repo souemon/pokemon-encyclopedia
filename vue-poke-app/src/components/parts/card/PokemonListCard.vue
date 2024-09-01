ae0fca1910ee15ac073a96e0bf9d4090ff2f863e
souemon <kobekobekitakita@gmail.com>
2024-09-01 09:06:15 +0900

    add: 一覧画面用のカードを作成

--- vue-poke-app/src/components/parts/card/PokemonListCard.vue
+++ vue-poke-app/src/components/parts/card/PokemonListCard.vue
@@ -1,3 +1,28 @@
-<script setup lang="ts"></script>
-<template></template>
-<style scoped></style>
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps(["name", "id", "image"]);
+</script>
+
+<template>
+  <v-card
+    class="pt-2 mx-2 mt-4"
+    min-width="150"
+    :to="{ name: 'detail', params: { id: props.id } }"
+  >
+    <v-card-subtitle class="text-subtitle-2 font-weight-medium"
+      >No.{{ props.id }}</v-card-subtitle
+    >
+    <v-card-title class="text-subtitle-1 font-weight-bold">{{
+      props.name
+    }}</v-card-title>
+    <v-img class="mx-auto mt-1 mb-4" width="130px" :src="props.image"></v-img>
+  </v-card>
+</template>
+
+<style scoped>
+.v-card-subtitle {
+  color: black !important;
+  opacity: 1;
+}
+</style>
