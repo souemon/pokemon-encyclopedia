bb39b0a63d21923fd172eb4434b5ce17d225394c
souemon <kobekobekitakita@gmail.com>
2024-09-14 14:57:08 +0900

    add: GitHub Pages用の設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue-poke-app/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
