ffa9e9ee166a50af5586d2054384d1583e0b633f
souemon <kobekobekitakita@gmail.com>
2024-10-10 08:46:41 +0900

    test: PokemonListCard.vueのテストケースを追加

--- tests/spec/components/parts/card/PokemonListCard.spec.ts
+++ tests/spec/components/parts/card/PokemonListCard.spec.ts
@@ -3,6 +3,7 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import PokemonListCard from "@parts/card/PokemonListCard.vue";
+import router from "@/router";
 
 const vuetify = createVuetify({
   components,
@@ -22,16 +23,26 @@ describe("PokemonListCard", () => {
       image,
     },
     global: {
-      plugins: [vuetify],
+      plugins: [vuetify, router],
     },
   });
+
   test("propsで受け取った「図鑑No.」を表示していること", () => {
     expect(wrapper.get(".v-card-subtitle").text()).toMatch(`No.${id}`);
   });
+
   test("propsで受け取った「名前」を表示していること", () => {
     expect(wrapper.get(".v-card-title").text()).toMatch(name);
   });
+
   test("propsで受け取った「画像」を表示していること", () => {
     expect(wrapper.get("img").attributes("src")).toMatch(image);
+  });
+
+  // もう少し良いテスト方法があれば知りたい・・・（うまく書けなかったです）
+  test("カードのURLが正しい遷移先のURLとなっていること", async () => {
+    expect(wrapper.get("a").attributes("href")).toBe(
+      `/vue-poke-app/detail/${id}`
+    );
   });
 });
